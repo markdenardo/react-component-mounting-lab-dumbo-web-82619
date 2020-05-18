c2ef5b7aa5707ea87d2d666c8c74a13400c2401a
markdenardo <mark.denardo@gmail.com>
2020-05-18 16:19:10 -0400

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -2,45 +2,64 @@ import React, { Component } from 'react';
 
 class Timer extends Component {
 
-  state = {
-    time: 0,
-    color: '#'+Math.floor(Math.random()*16777215).toString(16)
+  constructor() {
+    super()
+    this.timer = React.createRef()
+    this.state = {
+      time: 0,
+      color: '#' + Math.floor(Math.random() * 16777215).toString(16),
+    }
   }
 
-  // add your code here
 
-  componentDidMount(){
-    this.clockTick()
+
+  //Your code here
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
   }
 
-  componentWillUnmount(){
-    this.stopClock()
+  componentDidUpdate() {
+    this.timer.current.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16)
+    this.timer.current.style.width = 240 + this.state.time * 5 / 1000 + "px"
+    this.timer.current.style.height = 150 + this.state.time * 5 / 1000 + "px"
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, this.props.updateInterval * 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
   }
 
   render() {
-
-    const { time, color, className } = this.state
+    const { time, color, className, logText } = this.state
     return (
-      <section className="Timer" style={{background: color}}>
+      <section className="Timer" style={{ background: color }} ref={this.timer}>
 
-        <h1>{ time }</h1>
-        <button onClick={ this.stopClock }>Stop</button>
-        <aside className="mountText">Mounted</aside>
-        <small onClick={ this.handleClose }>X</small>
+        <h1>{time}</h1>
+        <button onClick={this.stopClock}>Stop</button>
+        <aside className="logText">{logText}</aside>
+        <small onClick={this.handleClose}>X</small>
 
       </section>
     );
   }
 
-  //clock functions
+
   clockTick = () => {
     this.setState(prevState => ({
-      time: prevState.time+1
+      time: prevState.time + this.props.updateInterval
     }))
   }
 
   stopClock = () => {
     clearInterval(this.interval)
+    this.setState({ className: "hidden" })
   }
 
   // for the 'x' button,
